d7403a2fd4604b9be494cee7f18b17b2bca92172
and <andrewcshike@gmail.com>
2021-03-11 10:18:28 +0700

    include friendly

--- orgn.lua
+++ orgn.lua
@@ -3,26 +3,26 @@
 function r() norns.script.load(norns.state.script) end
 
 mu = require 'musicutil'
-orgn = include 'lib/orgn'
+orgn = include 'orgn/lib/orgn'
 
 engine.name = "Orgn"
 engine.list_commands()
 
 m = midi.connect()
+m.event = function(data)
+    local msg = midi.to_msg(data)
+    if msg.type == "note_on" then
+        engine.noteOn(msg.note, mu.note_num_to_freq(msg.note), 1)
+    elseif msg.type == "note_off" then
+        engine.noteOff(msg.note)
+    end
+end
+
+orgn.params.synth('all', 'asr', 'linked');
+orgn.params.ulaw('complex')
 
 function init()
-    orgn.params.synth('all', 'asr', 'linked');
-    orgn.params.ulaw('complex')
     --params:read()
-
-    m.event = function(data)
-        local msg = midi.to_msg(data)
-        if msg.type == "note_on" then
-            engine.noteOn(msg.note, mu.note_num_to_freq(msg.note), 1)
-        elseif msg.type == "note_off" then
-            engine.noteOff(msg.note)
-        end
-    end
 end
 
 function cleanup() 
